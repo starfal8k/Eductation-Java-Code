ae189a1c4a2c38e54fc44f4384f8711b1eedd791
starfal8k <158791195+starfal8k@users.noreply.github.com>
2024-02-06 16:11:36 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var siteConfig = {
   nameSite: "Education Java Code",
   nameH1: "Education Java Code",
-  version: "0.0.2 beta 6"
+  version: "0.0.3 build 1"
 }
---
+++ messange.js
@@ -0,0 +1,4 @@
+function Onl() {
+  alert("Библиотеки\n________\n- Prism -\n________");
+  console.log('Messange Loaded');
+}
