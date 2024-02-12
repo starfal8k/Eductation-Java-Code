e44586e3451a28d71024f33638223b2d62c8e8ca
starfal8k <158791195+starfal8k@users.noreply.github.com>
2024-02-12 05:12:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+  
+  //Установка конфига сайта
+  document.getElementById('nameSite').textContent = siteConfig.nameSite;
+  document.getElementById('nameH1').textContent = siteConfig.nameH1;
+  document.getElementById('version').textContent = siteConfig.version;
+});
