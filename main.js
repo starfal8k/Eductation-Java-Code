3c003db1bfe988cb6384ab5d036ae3cb9106ed56
starfal8k <158791195+starfal8k@users.noreply.github.com>
2024-02-12 05:10:21 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  
-  //Установка конфига сайта
-  document.getElementById('nameSite').textContent = siteConfig.nameSite;
-  document.getElementById('nameH1').textContent = siteConfig.nameH1;
-  document.getElementById('version').textContent = siteConfig.version;
-});
